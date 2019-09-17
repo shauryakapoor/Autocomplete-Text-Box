21234952f47a19e01f92567063f450acb51b1e2e
Shaurya Kapoor <shauryakapoor@eduroam-169-233-179-250.ucsc.edu>
2019-09-17 10:42:28 -0700

    When I type in box, only items that match the value get shown, also suggestions are hidden now

--- src/AutoCompleteText.js
+++ src/AutoCompleteText.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { RegExp } from 'core-js';
 
 export default class AutoCompleteText extends React.Component {
     constructor (props) {
@@ -10,15 +11,38 @@ export default class AutoCompleteText extends React.Component {
             'Sara',
             'Jane',
         ];
+        this.state = {
+            suggestions: [],
+        };
+    }
+
+    onTextChanged = (e) => {
+        const value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0){
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.items.sort().filter(v => v.test(regex));
+        }
+        this.setState(() => ({ suggestions }));
+    }
+
+    renderSuggestions () {
+        const { suggestions } = this.state;
+        if ( suggestions.length === 0) {
+            return null;
+        }
+        return (
+            <ul>
+                {this.suggestions.map((item) => <li>{item}</li>)}
+            </ul>
+        )
     }
 
     render () {
         return (
             <div>
-                <input type="text"/>
-                <ul>
-                    {this.items.map((item) => <li>{item}</li>)}
-                </ul>
+                <input onChange={this.onTextChanged} type="text"/>
+                {this.renderSuggestions()}
             </div>
         )
     }
