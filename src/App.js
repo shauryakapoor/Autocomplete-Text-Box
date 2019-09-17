817bb7eea3118dc0919ba4a1a420b7f496000f15
Shaurya Kapoor <shauryakapoor@eduroam-169-233-179-250.ucsc.edu>
2019-09-17 09:46:48 -0700

    build a toggle with dynamic Text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HideableText from './HideableText';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">Welcome to React</h1>
+        <div className="App-content">
+          <HideableText text="Dynamic Text!" />
+      </div>
       </header>
     </div>
   );
